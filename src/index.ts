d941cff56d1d5bb0c7b428840b89387e66dc338b
danricardo88 <daniel.ricardo88@outlook.com>
2023-02-15 18:09:28 -0300

    meio perdido

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import app from './app';
+import connectToDatabase from './Models/Connection';
+
+const DEFAULT_PORT = 3001;
+
+async function startServer() {
+  try {
+    await connectToDatabase();
+    const port = process.env.PORT || DEFAULT_PORT;
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  } catch (error) {
+    console.log('Connection with database generated an error:\r\n');
+    console.error(error);
+    console.log('\r\nServer initialization cancelled');
+    process.exit(1);
+  }
+}
+
+startServer();
