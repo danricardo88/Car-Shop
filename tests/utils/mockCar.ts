ddc5f4bfb30c2b6d0c2d3eb5004ade6a0004495e
danricardo88 <daniel.ricardo88@outlook.com>
2023-02-16 20:27:05 -0300

    fim

---
+++ tests/utils/mockCar.ts
@@ -0,0 +1,32 @@
+import Car from '../../src/Domains/Car';
+import ICar from '../../src/Interfaces/ICar';
+import HttpException from '../../src/Util/HttpExceptions';
+
+const carInput: ICar = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const carOutput: Car = new Car({
+  id: '6378d1c99b090153e49a4895',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+});
+
+const genericError : HttpException = new HttpException(500, 'Internal Server Error');
+
+export {
+  carInput,
+  carOutput,
+  genericError,
+};
