75cc360219ea9b7130781ee6e87afa521d98faa4
danricardo88 <daniel.ricardo88@outlook.com>
2023-02-16 20:21:53 -0300

    programando de um jeito diferente

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import routes from './router';
+import HandleError from './middleware/handleError';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(HandleError.handle);
 
 export default app;
